6da82a757d919b3c25bf195ece6c4ae951238e44
Rodrigo Moraes <rodrigobatsmoraes@hotmail.com>
2021-11-09 21:31:54 -0300

    replace wasmer by the more lightweight wasmi

--- chasm-rs/src/run_wasm.rs
+++ chasm-rs/src/run_wasm.rs
@@ -1,7 +1,10 @@
+use core::panic;
 use std::fmt::Write;
 use std::sync::{Arc, Mutex};
 
-use wasmer::{Function, Instance, Memory, MemoryType, Module, Store, imports};
+use wasmi::memory_units::Pages;
+use wasmi::nan_preserving_float::F32;
+use wasmi::{Externals, FuncInstance, ImportsBuilder, Module, ModuleImportResolver, ModuleInstance, RuntimeValue, Signature, ValueType, Error as InterpreterError, MemoryInstance};
 
 pub fn dump_hex(data: &[u8]) {
     let mut bytes = data;
@@ -36,7 +39,44 @@ pub fn dump_hex(data: &[u8]) {
         offset += D;
     }
 }
-use wasmer::WasmerEnv;
+
+struct EnvModuleResolver(wasmi::MemoryRef);
+impl ModuleImportResolver for EnvModuleResolver {
+    fn resolve_func(&self, field_name: &str, _signature: &wasmi::Signature) -> Result<wasmi::FuncRef, wasmi::Error> {
+        let func = match field_name {
+            "print" => FuncInstance::alloc_host(Signature::new(&[ValueType::F32][..], None), 0),
+            _ => {
+                return Err(InterpreterError::Function(format!(
+                    "host module doesn't export function with name {}",
+                    field_name
+                )));
+            }
+        };
+        Ok(func)
+    }
+
+    fn resolve_memory(&self, field_name: &str, _memory_type: &wasmi::MemoryDescriptor) -> Result<wasmi::MemoryRef, InterpreterError> {
+        let mem = match field_name {
+            "memory" => self.0.clone(),
+            _ => panic!("HAHAHAH!!"),
+        };
+        Ok(mem)
+    }
+}
+
+struct Runtime<W: Write>(Arc<Mutex<W>>);
+impl<W: Write> Externals for Runtime<W> {
+    fn invoke_index(&mut self, index: usize, args: wasmi::RuntimeArgs) -> Result<Option<RuntimeValue>, wasmi::Trap> {
+        match index {
+            0 => {
+                let n: f32 = args.nth::<F32>(0).into();
+                writeln!(self.0.lock().unwrap(), "{}", n).unwrap();
+            },
+            _ => panic!("HAHAHAH!!!")
+        };
+        Ok(None)
+    }
+}
 
 pub fn run_binary<W: Write + Send + 'static>(
     binary: &[u8],
@@ -44,31 +84,23 @@ pub fn run_binary<W: Write + Send + 'static>(
 ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     dump_hex(&binary);
 
-    struct Writer<W: Send> {
-        w: Arc<Mutex<W>>,
-    }
-    impl<W: Send> WasmerEnv for Writer<W> {}
-    impl<W: Send> Clone for Writer<W> {
-        fn clone(&self) -> Self {
-            Self { w: self.w.clone() }
-        }
-    }
+    let mut runtime = Runtime(out);
 
-    let writer = Writer { w: out };
-
-    let store = Store::default();
-    let module = Module::new(&store, &binary)?;
-    let memory = Memory::new(&store, MemoryType::new(1, None, false)).unwrap();
-    let import_object = imports! {
-        "env" => {
-            "print" => Function::new_native_with_env(&store, writer, |out: &Writer<W>, x: f32| writeln!(&mut *out.w.lock().unwrap(), "{}", x)),
-            "memory" => memory.clone(),
-        }
-    };
-    let instance = Instance::new(&module, &import_object)?;
-    let main = instance.exports.get_function("main")?;
-    main.call(&[])?;
-    let mut data = unsafe { memory.data_unchecked() }.to_owned();
+    let module = Module::from_buffer(&binary)?;
+    // let memory = Memory::new(&store, MemoryType::new(1, None, false)).unwrap();
+    // let import_object = imports! {
+    //     "env" => {
+    //         "print" => Function::new_native_with_env(&store, writer, |out: &Writer<W>, x: f32| writeln!(&mut *out.w.lock().unwrap(), "{}", x)),
+    //         "memory" => memory.clone(),
+    //     }
+    // };
+    let memory = MemoryInstance::alloc(Pages(1), None).unwrap();
+    let resolver = &EnvModuleResolver(memory.clone());
+    let import_object = ImportsBuilder::default()
+        .with_resolver("env", resolver);
+    let instance = ModuleInstance::new(&module, &import_object)?.assert_no_start();
+    instance.invoke_export("main", &[], &mut runtime)?;
+    let mut data = memory.direct_access().as_ref().to_owned();
     data.resize(100*100, 0);
     Ok(data)
 }
